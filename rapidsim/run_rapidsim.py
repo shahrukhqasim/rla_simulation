29a90ca3925ba35efc0de323aa239e320aa004ea
Alex Marshall <alex.marshall@bristol.ac.uk>
2023-08-16 14:18:04 +0200

    rapidsim all the combinations of particles from set of mothers/daughters

--- rapidsim/run_rapidsim.py
+++ rapidsim/run_rapidsim.py
@@ -9,9 +9,13 @@ import numpy as np
 import yaml
 from particle import Particle
 from rlasim.lib.data_core import get_pdgid
+import pandas as pd
 
+# from particle import Particle
+# print(Particle.from_pdgid(-12))
+# quit()
 
-def run(config_file=None, section=None, dont_clean=False):
+def run(config_file=None, section=None, dont_clean=False, N_events=1E6):
     """
     Run RapidSim.
 
@@ -38,6 +42,83 @@ def run(config_file=None, section=None, dont_clean=False):
         unique_combinations = [list(comb) for comb in unique_combinations]
         mother_particles = ["B+"]
         num_sims = [10000 for _ in mother_particles]
+
+        decay_channels = {}
+
+        N_channels_total = 0
+        for mother_particle in mother_particles:
+            decay_channels[mother_particle] = []
+            for unique_combination in unique_combinations:
+                decay_channels[mother_particle].append(unique_combination)
+            N_channels_total += len(decay_channels[mother])
+
+
+    elif config_file == "produce_all":
+        
+        particles = pd.read_csv('particles.dat', skiprows=1, names=['ID', 'part', 'anti', 'mass', 'width', 'charge', 'spin', 'shape', 'ctau'], sep='\s+')
+
+        mother_particles = [
+                                "B0", "B+", "Bs0", "Bc+", 
+                                "D0", "D+", "Ds+"
+                            ]
+        daughter_particles = [
+                                "gamma",
+                                "K+", "pi+", "pi0",
+                                "e-", "mu-",
+                                "nue",
+                            ]
+
+        for index, row in particles.iterrows():
+            if row['part'] in daughter_particles:
+                if row['anti'] != '---':
+                    daughter_particles.append(row['anti'])
+
+        # Set the desired length of each combination
+        combination_length = 3
+
+        # Generate all combinations with repetitions allowed
+        all_combinations = list(itertools.product(daughter_particles, repeat=combination_length))
+
+        # Remove duplicates by converting each combination to a set
+        unique_combinations = {tuple(sorted(comb)) for comb in all_combinations}
+        unique_combinations = [(x[0], x[1], x[2]) for x in unique_combinations]
+
+        # Convert back to a list of lists
+        unique_combinations = [list(comb) for comb in unique_combinations]
+
+        N_channels_total = 0
+
+        decay_channels = {}
+
+        for mother in mother_particles:
+
+            print(f'\nComputing decay channels for {mother}...')
+
+            mother_info = particles[particles['part'] == mother]
+
+            decay_channels[mother] = []
+
+            for unique_combination in unique_combinations:
+                
+                daughters_lists = {}
+                daughters_lists['masses'] =[]
+                daughters_lists['charges'] =[]
+
+                for particle_i in unique_combination:
+                    daughter_info = particles[particles['part'] == particle_i]
+                    if daughter_info.shape[0] == 0:
+                        daughter_info = particles[particles['anti'] == particle_i]
+                    daughters_lists['masses'].append(daughter_info.mass.item())
+                    daughters_lists['charges'].append(daughter_info.charge.item())
+
+                if mother_info.mass.item() > sum(daughters_lists['masses']) and sum(daughters_lists['charges']) == mother_info.charge.item():
+                    decay_channels[mother].append(unique_combination)
+
+            print(f'{len(decay_channels[mother])} decay channels listed.')
+            N_channels_total += len(decay_channels[mother])
+
+        print(f'\nN_channels_total: {N_channels_total}\n\n')
+
     else:
         assert section is not None
         try:
@@ -47,11 +128,19 @@ def run(config_file=None, section=None, dont_clean=False):
             print(exc)
             exit()
 
+        decay_channels = {}
+
         decays = config[section]['decays']
         unique_combinations = [x['daughters'] for x in decays]
         mother_particles = [x['mother'] for x in decays]
         num_sims = [x['num_sims'] for x in decays]
 
+        N_channels_total = 0
+        for mother_particle in mother_particles:
+            decay_channels[mother_particle] = []
+            for unique_combination in unique_combinations:
+                decay_channels[mother_particle].append(unique_combination)
+            N_channels_total += len(decay_channels[mother])
 
     try:
         os.system('rm -r rs_output')
@@ -61,74 +150,81 @@ def run(config_file=None, section=None, dont_clean=False):
     os.mkdir('rs_output')
     os.chdir('rs_output')
 
-    print(f"Running {len(unique_combinations)} channels")
+    print(f"Running {N_channels_total} channels")
     rapid_sim_path = '~/RapidSim/RapidSim/build/src/RapidSim.exe'
     if os.environ.get('RAPID_SIM_EXE_PATH') is not None:
         rapid_sim_path = os.environ.get('RAPID_SIM_EXE_PATH')
 
     rs_idx = -1
-    for particle_m, particle_combination in zip(mother_particles, unique_combinations):
-        particle_i = particle_combination[0]
-        particle_j = particle_combination[1]
-        particle_k = particle_combination[2]
 
-        rs_idx += 1
+    for particle_m in list(decay_channels.keys()):
+        for particle_combination in decay_channels[particle_m]:
 
-        f = open('../BLANK.config', 'r')
-        f_lines = f.readlines()
-        with open(f'rs_{rs_idx}.config', 'a') as f_out:
-            for idx, line in enumerate(f_lines):
-                if 'BLANK0' in line:
-                    line = line.replace('BLANK0', particle_m)
-                if 'BLANK1' in line:
-                    line = line.replace('BLANK1', particle_i)
-                if 'BLANK2' in line:
-                    line = line.replace('BLANK2', particle_j)
-                if 'BLANK3' in line:
-                    line = line.replace('BLANK3', particle_k)
-                f_out.write(line)
+            particle_i = particle_combination[0]
+            particle_j = particle_combination[1]
+            particle_k = particle_combination[2]
 
-        f = open('../BLANK.decay', 'r')
-        f_lines = f.readlines()
-        with open(f'rs_{rs_idx}.decay', 'a') as f_out:
-            for idx, line in enumerate(f_lines):
-                if 'BLANK0' in line:
-                    line = line.replace('BLANK0', particle_m)
-                if 'BLANK1' in line:
-                    line = line.replace('BLANK1', particle_i)
-                if 'BLANK2' in line:
-                    line = line.replace('BLANK2', particle_j)
-                if 'BLANK3' in line:
-                    line = line.replace('BLANK3', particle_k)
-                f_out.write(line)
+            rs_idx += 1
+
+            f = open('../BLANK.config', 'r')
+            f_lines = f.readlines()
+            with open(f'rs_{rs_idx}.config', 'a') as f_out:
+                for idx, line in enumerate(f_lines):
+                    if 'BLANK0' in line:
+                        line = line.replace('BLANK0', particle_m)
+                    if 'BLANK1' in line:
+                        line = line.replace('BLANK1', particle_i)
+                    if 'BLANK2' in line:
+                        line = line.replace('BLANK2', particle_j)
+                    if 'BLANK3' in line:
+                        line = line.replace('BLANK3', particle_k)
+                    f_out.write(line)
+
+            f = open('../BLANK.decay', 'r')
+            f_lines = f.readlines()
+            with open(f'rs_{rs_idx}.decay', 'a') as f_out:
+                for idx, line in enumerate(f_lines):
+                    if 'BLANK0' in line:
+                        line = line.replace('BLANK0', particle_m)
+                    if 'BLANK1' in line:
+                        line = line.replace('BLANK1', particle_i)
+                    if 'BLANK2' in line:
+                        line = line.replace('BLANK2', particle_j)
+                    if 'BLANK3' in line:
+                        line = line.replace('BLANK3', particle_k)
+                    f_out.write(line)
 
     time_A_full = time.time()
     rs_idx = -1
-    for particle_m, particle_combination, N in zip(mother_particles, unique_combinations, num_sims):
-        particle_i = particle_combination[0]
-        particle_j = particle_combination[1]
-        particle_k = particle_combination[2]
+    for particle_m in list(decay_channels.keys()):
+        for particle_combination in decay_channels[particle_m]:
 
-        rs_idx += 1
-        print(
-            f"{rs_idx + 1}/{len(unique_combinations)}, Running RapidSim for {particle_m} -> {particle_i} {particle_j} {particle_k}")
-        time_A = time.time()
-        os.system(f'{rapid_sim_path} rs_{rs_idx} {N} 1 > dump.txt  ')
-        file = uproot.open(f'rs_{rs_idx}_tree.root')["DecayTree"]
-        keys = file.keys()
-        results = file.arrays(keys, library="np")
-        file.close()
+            N = int(N_events/N_channels_total)
 
-        results['mother_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_m)
-        results['particle_1_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_i)
-        results['particle_2_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_j)
-        results['particle_3_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_k)
+            particle_i = particle_combination[0]
+            particle_j = particle_combination[1]
+            particle_k = particle_combination[2]
 
-        file2 = uproot.recreate(f'rs_{rs_idx}_tree2.root')
-        file2['DecayTree'] = results
-        file2.close()
+            rs_idx += 1
+            print(
+                f"{rs_idx + 1}/{N_channels_total}, Running RapidSim for {particle_m} -> {particle_i} {particle_j} {particle_k}")
+            time_A = time.time()
+            os.system(f'{rapid_sim_path} rs_{rs_idx} {N} 1 > dump.txt  ')
+            file = uproot.open(f'rs_{rs_idx}_tree.root')["DecayTree"]
+            keys = file.keys()
+            results = file.arrays(keys, library="np")
+            file.close()
 
-        time_B = time.time()
+            results['mother_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_m)
+            results['particle_1_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_i)
+            results['particle_2_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_j)
+            results['particle_3_PID'] = np.zeros(len(results['mother_E']), dtype=np.int32) + get_pdgid(particle_k)
+
+            file2 = uproot.recreate(f'rs_{rs_idx}_tree2.root')
+            file2['DecayTree'] = results
+            file2.close()
+
+            time_B = time.time()
 
     time_B_full = time.time()
 
--- rlasim/lib/data_core.py
+++ rlasim/lib/data_core.py
@@ -1,5 +1,10 @@
-import torch
-from torch.utils.data import Dataset, TensorDataset
+torch_installed = True
+try:
+    import torch
+    from torch.utils.data import Dataset, TensorDataset
+except:
+    torch_installed = False
+    pass
 import numpy as np
 from particle import Particle
 import warnings
@@ -9,6 +14,8 @@ def get_pdgid(name_):
     rapidsim2pdg_mapping = {
         'p+': 'p',
         'p-': 'p~',
+        'nue': 'nu(e)',
+        'anti-nue': 'nu(e)~',
     }
 
     if name_ in rapidsim2pdg_mapping:
@@ -26,43 +33,46 @@ def get_pdgid(name_):
 
     return int(respective[0].pdgid)
 
-def tensors_dict_join(list_of_dicts):
-    collected_tensors = {}
+if torch_installed:
+    
+    def tensors_dict_join(list_of_dicts):
+        collected_tensors = {}
 
-    for dictionary in list_of_dicts:
-        for key, tensor in dictionary.items():
-            if key in collected_tensors:
-                collected_tensors[key].append(tensor)
+        for dictionary in list_of_dicts:
+            for key, tensor in dictionary.items():
+                if key in collected_tensors:
+                    collected_tensors[key].append(tensor)
+                else:
+                    collected_tensors[key] = [tensor]
+
+        concatenated_dict = {}
+
+        for key, tensor_list in collected_tensors.items():
+            if isinstance(tensor_list[0], np.ndarray):
+                concatenated_dict[key] = np.concatenate(tensor_list, axis=0)
             else:
-                collected_tensors[key] = [tensor]
+                concatenated_dict[key] = torch.cat(tensor_list, dim=0)
 
-    concatenated_dict = {}
+        return concatenated_dict
 
-    for key, tensor_list in collected_tensors.items():
-        if isinstance(tensor_list[0], np.ndarray):
-            concatenated_dict[key] = np.concatenate(tensor_list, axis=0)
-        else:
-            concatenated_dict[key] = torch.cat(tensor_list, dim=0)
+    class DictTensorDataset(Dataset):
+        def __init__(self, dict: dict):
+            self.dataset_dict = {}
+            lens = set()
+            self.one_key = None
+            for k, v in dict.items():
+                assert type(v) is not list
+                self.dataset_dict[k] = TensorDataset(v)
+                lens = lens.union({len(v)})
+                self.one_key = k
 
-    return concatenated_dict
-class DictTensorDataset(Dataset):
-    def __init__(self, dict: dict):
-        self.dataset_dict = {}
-        lens = set()
-        self.one_key = None
-        for k, v in dict.items():
-            assert type(v) is not list
-            self.dataset_dict[k] = TensorDataset(v)
-            lens = lens.union({len(v)})
-            self.one_key = k
+            assert len(lens) == 1
 
-        assert len(lens) == 1
+        def __getitem__(self, item):
+            return_dict = {}
+            for k,v in self.dataset_dict.items():
+                return_dict[k] = v[item][0] # It returns output as a list
+            return return_dict
 
-    def __getitem__(self, item):
-        return_dict = {}
-        for k,v in self.dataset_dict.items():
-            return_dict[k] = v[item][0] # It returns output as a list
-        return return_dict
-
-    def __len__(self):
-        return len(self.dataset_dict[self.one_key])
+        def __len__(self):
+            return len(self.dataset_dict[self.one_key])
